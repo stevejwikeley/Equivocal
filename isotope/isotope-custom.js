b8b0338bf856a11c2f2769bc1bfdd1370a062c11
stevejwikeley <stevewikeley@Steves-MacBook-Pro.local>
2014-10-14 12:35:00 +0100

    Reverted to back-up

--- isotope/isotope-custom.js
+++ isotope/isotope-custom.js
@@ -1,27 +1,6 @@
 jQuery(window).load(function(){
 
-	var $container = $('.        <section id="hero" class="tbg1">
-            <div id="layerslider-container-fw">
-               <div id="layerslider" style="width: 100%; height: 530px; margin: 0px auto;">
-    
-                  <div class="ls-layer" style="slidedirection: left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-01.jpg" class="ls-bg" alt="">
-                     <div class="ls-s1 " style=" position: absolute; top:80%; left: 50%;  slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                       </div>                        
-                  </div>
-
-                  <div class="ls-layer" style=" slideoutdirection : left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-02.jpg" class="ls-bg" alt=""> 
-
-                     <div class="ls-s1" style=" position: absolute; top:80%; left: 50%; slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                     </div>
-                  </div>
-
-                 
-                  
-                  </div>
-               </div>
-slide-picslio');
+	var $container = $('.portfolio');
 	$container.isotope({
 		filter: '*',
 		//sortBy : 'random',
@@ -32,28 +11,7 @@ slide-picslio');
 		}
 	});
 
-	$('nav.primary .        <section id="hero" class="tbg1">
-            <div id="layerslider-container-fw">
-               <div id="layerslider" style="width: 100%; height: 530px; margin: 0px auto;">
-    
-                  <div class="ls-layer" style="slidedirection: left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-01.jpg" class="ls-bg" alt="">
-                     <div class="ls-s1 " style=" position: absolute; top:80%; left: 50%;  slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                       </div>                        
-                  </div>
-
-                  <div class="ls-layer" style=" slideoutdirection : left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-02.jpg" class="ls-bg" alt=""> 
-
-                     <div class="ls-s1" style=" position: absolute; top:80%; left: 50%; slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                     </div>
-                  </div>
-
-                 
-                  
-                  </div>
-               </div>
-slide-picslioFilters a').click(function(){
+	$('nav.primary .portfolioFilters a').click(function(){
 		var selector = $(this).attr('data-filter');
 		$container.isotope({
 			filter: selector,
@@ -66,28 +24,7 @@ slide-picslioFilters a').click(function(){
 	  return false;
 	});
 
-	var $optionSets = $('nav.primary .        <section id="hero" class="tbg1">
-            <div id="layerslider-container-fw">
-               <div id="layerslider" style="width: 100%; height: 530px; margin: 0px auto;">
-    
-                  <div class="ls-layer" style="slidedirection: left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-01.jpg" class="ls-bg" alt="">
-                     <div class="ls-s1 " style=" position: absolute; top:80%; left: 50%;  slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                       </div>                        
-                  </div>
-
-                  <div class="ls-layer" style=" slideoutdirection : left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-02.jpg" class="ls-bg" alt=""> 
-
-                     <div class="ls-s1" style=" position: absolute; top:80%; left: 50%; slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                     </div>
-                  </div>
-
-                 
-                  
-                  </div>
-               </div>
-slide-picslioFilters'),
+	var $optionSets = $('nav.primary .portfolioFilters'),
 	       $optionLinks = $optionSets.find('a');
 	 
 	       $optionLinks.click(function(){
@@ -96,28 +33,7 @@ slide-picslioFilters'),
 		  if ( $this.hasClass('selected') ) {
 		      return false;
 		  }
-	   var $optionSet = $this.parents('nav.primary .        <section id="hero" class="tbg1">
-            <div id="layerslider-container-fw">
-               <div id="layerslider" style="width: 100%; height: 530px; margin: 0px auto;">
-    
-                  <div class="ls-layer" style="slidedirection: left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-01.jpg" class="ls-bg" alt="">
-                     <div class="ls-s1 " style=" position: absolute; top:80%; left: 50%;  slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                       </div>                        
-                  </div>
-
-                  <div class="ls-layer" style=" slideoutdirection : left; slidedelay: 5000; durationin: 1500; durationout: 1500;">
-                     <img src="images/slide-pics/dovecote-02.jpg" class="ls-bg" alt=""> 
-
-                     <div class="ls-s1" style=" position: absolute; top:80%; left: 50%; slideoutdirection : left; durationin : 0; durationout : 750; easingin : easeInOutQuint; easingout : easeInBack; delayin : 400;">
-                     </div>
-                  </div>
-
-                 
-                  
-                  </div>
-               </div>
-slide-picslioFilters');
+	   var $optionSet = $this.parents('nav.primary .portfolioFilters');
 	   $optionSet.find('.selected').removeClass('selected');
 	   $this.addClass('selected'); 
 	});
